cd5fff295213cdfe70fbc76ec9c6dddd19139566
sergiokas <sergiokas@gmail.com>
2014-04-03 19:37:26 -0300

    fixing indentation

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ BASE="https://raw.githubusercontent.com/sergiokas/bashfiles/master"
 function getconf {
 	if [ ! -f ~/$1 ]; then
     echo "Updating $1..."
-		curl -sSL $BASE/$1 > ~/$1
-	else
-		echo "$1 already exists, skipping."
-	fi
+    curl -sSL $BASE/$1 > ~/$1
+  else
+    echo "$1 already exists, skipping."
+  fi
 }
 
 getconf .vimrc
